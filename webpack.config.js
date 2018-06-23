df4e41bca983e7e090a86287370302f3da0c9010
Pedro Martin <pataruco@gmail.com>
2018-06-23 22:23:36 +0100

    Add assets and eslint to webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -9,37 +9,42 @@ const postcssPresetEnv = require('postcss-preset-env')({
   },
 });
 const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
 
 module.exports = {
   entry: [
-    // We ship a few polyfills by default:
     require.resolve('./config/polyfills'),
-    // Include an alternative client for WebpackDevServer. A client's job is to
-    // connect to WebpackDevServer by a socket and get notified about changes.
-    // When you save a file, the client will either apply hot updates (in case
-    // of CSS changes), or refresh the page (in case of JS changes). When you
-    // make a syntax error, this client will display a syntax error overlay.
-    // Note: instead of the default WebpackDevServer client, we use a custom one
-    // to bring better experience for Create React App users. You can replace
-    // the line below with these two lines if you prefer the stock client:
-    // require.resolve('webpack-dev-server/client') + '?/',
-    // require.resolve('webpack/hot/dev-server')
-    // require.resolve('react-dev-utils/webpackHotDevClient'),
-    // Finally, this is your app's code:
-    // paths.appIndexJs
-    // We include the app code last so that if there is a runtime error during
-    // initialization, it doesn't blow up the WebpackDevServer client, and
-    // changing JS code would still trigger a refresh.
+    require.resolve('react-dev-utils/webpackHotDevClient'),
   ],
-  // entry: [fetch, './src/index.js'],
   mode: 'development',
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint'),
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+      },
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
         options: { presets: ['@babel/env'] },
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
       },
       {
         test: /\.css$/,
