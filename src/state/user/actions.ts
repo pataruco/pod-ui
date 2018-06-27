80d33adc63a58f151bc55c7f47f4aa44fc5aa4f0
Pedro Martin <pataruco@gmail.com>
2018-06-27 21:20:08 +0100

    made request to manifest

--- src/state/user/actions.ts
+++ src/state/user/actions.ts
@@ -1,10 +1,18 @@
 import { createAction } from 'redux-actions';
-
+import 'whatwg-fetch';
 import { END_DATA_FETCH, START_DATA_FETCH } from './action-types';
 
 export const endDataFetch = createAction(END_DATA_FETCH);
 export const startDataFetch = createAction(START_DATA_FETCH);
+const podData =
+  'https://peter-of-the-day.s3.amazonaws.com/production/manifest/manifest.json';
 
-export const fetchData = () => {
+export const fetchData = async () => {
   // console.log('fetchData');
+  try {
+    const request = await fetch(podData);
+    const response = await request.json();
+  } catch (error) {
+    throw new Error(error);
+  }
 };
