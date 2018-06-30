481d5088ea855acdbc92ee939c40991495f1ba8e
Pedro Martin <pataruco@gmail.com>
2018-06-30 08:58:19 +0100

    Test connected Calendar component

--- src/components/calendar/index.test.tsx
+++ src/components/calendar/index.test.tsx
@@ -1,6 +1,8 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import { CalendarComponent } from './index';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import Calendar, { CalendarComponent } from './index';
 
 describe('CalendarComponent', () => {
   const defaultProps = {
@@ -10,5 +12,23 @@ describe('CalendarComponent', () => {
 
   it('renders', () => {
     expect(shallow(<CalendarComponent {...defaultProps} />)).toMatchSnapshot();
+  });
+});
+
+describe('Calendar', () => {
+  const defaultProps = {
+    dates: {},
+    dataIsLoaded: true,
+  };
+
+  it('renders', () => {
+    const store = configureStore([])({});
+    expect(
+      shallow(
+        <Provider store={store}>
+          <Calendar {...defaultProps} />
+        </Provider>,
+      ),
+    ).toMatchSnapshot();
   });
 });
