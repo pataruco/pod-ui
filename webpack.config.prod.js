e097400e34facb2522ca0b0c1ba6c32a5c1e04cf
Pedro Martin <pataruco@gmail.com>
2018-07-03 11:40:42 +0100

    Create a webpack config for production build

---
+++ webpack.config.prod.js
@@ -0,0 +1,88 @@
+const autoprefixer = require('autoprefixer');
+const path = require('path');
+const webpack = require('webpack');
+const postcssImport = require('postcss-import');
+const postcssMixins = require('postcss-mixins');
+const postcssPresetEnv = require('postcss-preset-env')({
+  features: {
+    autoprefixer: false,
+  },
+});
+const postcssFlexbugsFixes = require('postcss-flexbugs-fixes');
+const eslintFormatter = require('react-dev-utils/eslintFormatter');
+require('url-loader');
+require('file-loader');
+
+module.exports = {
+  entry: [require.resolve('./config/polyfills'), './src/index.js'],
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        enforce: 'pre',
+        use: [
+          {
+            options: {
+              formatter: eslintFormatter,
+              eslintPath: require.resolve('eslint'),
+            },
+            loader: require.resolve('eslint-loader'),
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] },
+      },
+      {
+        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+        loader: require.resolve('url-loader'),
+        options: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      },
+      {
+        test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+        loader: 'url-loader?limit=30000&name=[name]-[hash].[ext]',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('typings-for-css-modules-loader'),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              namedExport: true,
+              localIdentName: '[folder]-[local]-[hash:base64:5]',
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                postcssImport,
+                postcssMixins,
+                postcssPresetEnv,
+                postcssFlexbugsFixes,
+                autoprefixer(),
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+};
